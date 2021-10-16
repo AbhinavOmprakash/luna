bb969b6e7e156c504910ad0a35a2cfa1d184e60f
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2021-10-16 17:28:22 +0530

    added repo url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject luna "0.1.0-SNAPSHOT"
   :description "A Domain Specific Language (DSL) that translates to regex.Pattern"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/AbhinavOmprakash/luna"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]]
