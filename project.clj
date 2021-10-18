c0629069db6667fe489a84c5050ec3a68a4e35de
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2021-10-18 11:25:23 +0530

    update

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject luna "0.1.0-SNAPSHOT"
-  :description "A Domain Specific Language (DSL) that translates to regex.Pattern"
+(defproject org.clojars.abhinav/luna "0.1.0-SNAPSHOT"
+  :description "A Domain Specific Language (DSL) that translates to regex."
   :url "https://github.com/AbhinavOmprakash/luna"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
