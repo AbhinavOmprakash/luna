6a9d68ae901925bbeaae667e2c462821101caea9
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2021-11-10 19:17:15 +0530

    add cloverage

--- project.clj
+++ project.clj
@@ -5,5 +5,5 @@
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]]
   :plugins [[lein-auto "0.1.3"]
-            [lein-auto "0.1.3"]]
+            [lein-cloverage "1.0.9"]]
   :repl-options {:init-ns luna.core})
